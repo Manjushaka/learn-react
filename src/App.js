940cbb9abbcc8f942458122e1e1983afd685ba2b
沙丹 <dan.sha@datayes.com>
2018-03-29 20:42:39 +0800

    添加redux，写的一个加减的小demo，但是还没有找到好的让界面render的方法，只能手动设一个update state用于每次dispatch action之后render

--- src/App.js
+++ src/App.js
@@ -1,18 +1,51 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      update: false,
+    }
+    const { getState, dispatch, subscribe } = this.props.store;
+    this.unsubscribe = subscribe(() => {
+      this.setState(prevState => ({
+        update: !prevState.update,
+      }));
+    });
+  }
+
+  handleClickAdd = () => {
+        const { getState, dispatch, subscribe } = this.props.store;
+        dispatch({
+          type: 'ADD',
+        })
+  }
+
+  handleClickMinus = () => {
+        const { getState, dispatch, subscribe } = this.props.store;
+        dispatch({
+          type: 'MINUS',
+          payload: 2,
+        });
+  }
+
   render() {
+    const { getState, dispatch, subscribe } = this.props.store;
+    const { count } = getState();
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">hello world</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-intro">
+          <h1>count: {count}</h1>
+          <button onClick={this.handleClickAdd}>+1</button>
+          <button onClick={this.handleClickMinus}>-2</button>
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 import './index.css';
 import App from './App';
+import { countAddMinus } from './reducer';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(countAddMinus);
+store.subscribe(() => {
+    console.log('subscribe state: ', store.getState());
+})
+
+ReactDOM.render(<App store={store} />, document.getElementById('root'));
 registerServiceWorker();
