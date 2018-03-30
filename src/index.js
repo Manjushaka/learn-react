b8bccd2a6bbdb1291aa517e49b3e71c0998b7365
沙丹 <dan.sha@datayes.com>
2018-03-30 11:23:44 +0800

    学习middleware-添加redux-logger中间件，打印action信息，并和redux-devtools-extensions结合使用

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import logger from 'redux-logger';
+
 import './index.css';
 import App from './App';
 import reducer from './reducer';
 import registerServiceWorker from './registerServiceWorker';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
     reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    composeEnhancers(
+        applyMiddleware(logger),
+    ),
 );
-store.subscribe(() => {
-    console.log('subscribe state: ', store.getState());
-})
+// store.subscribe(() => {
+//     console.log('subscribe state: ', store.getState());
+// })
 
 ReactDOM.render(<App store={store} />, document.getElementById('root'));
 registerServiceWorker();
